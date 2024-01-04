0e9ececf4031485eb6f6a427ab252c1c7a72ce6a
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-01-04 22:29:59 +0530

    master

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
       </Canvas>
       <Overlay />
       </section>
+      
     </div>
   );
 }
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,10 +1,12 @@
 import { useProgress } from "@react-three/drei";
 import { usePlay } from "../contexts/Play";
 import data from '../data/data.json'
+import { useState } from "react";
 
 export const Overlay = () => {
   const { progress } = useProgress();
   const { play, end, setPlay, hasScroll } = usePlay();
+  const [homepage, setHomepage] = useState(false);
   return (
     <div
       className={`overlay ${play ? "overlay--disable" : ""}
@@ -18,7 +20,7 @@ export const Overlay = () => {
           <img src={data.logo} className="logo" alt=''/>
           <p className="intro__scroll">Scroll to begin the journey</p>
           <button
-            className="explore"
+            className="explore "
             onClick={() => {
               setPlay(true);
             }}
@@ -29,6 +31,12 @@ export const Overlay = () => {
       )}
       <div className={`outro ${end ? "outro--appear" : ""}`}>
         <p className="outro__text">Wish you had a great flight with us...</p>
+        <div className="contact__grid">
+        <a href='#homepage'><button className="knowMore__button github" >Github</button></a>
+        <a href='#homepage'><button className="knowMore__button linkedin" >Linkedin</button></a>
+        <a href='#homepage'><button className="knowMore__button twitter" >Twitter</button></a>
+        <a href='#homepage'><button className="knowMore__button portfolio" >Portfolio</button></a>
+        </div>
       </div>
     </div>
   );
